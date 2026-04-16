89d0183c89f2cd4a95b2f2e5edf86f9d9beee962
kitout3 <sella.quentin@gmail.com>
2026-04-16 12:26:20 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ⚠️ Remplace 'mariage-app' par le nom exact de ton repo GitHub
-// Exemple : si ton repo s'appelle "mariage-marie-thomas" → '/mariage-marie-thomas/'
-export default defineConfig({
-  plugins: [react()],
-  base: '/mariage-app/',
-})
